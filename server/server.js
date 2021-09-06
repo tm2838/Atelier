6ccc8a22e86f7f3348af0ece4b30d89ab34eb867
Surekha Weinberg <s.kaniget@gmail.com>
2021-09-05 17:44:49 -0700

    install dotenv, create .env file, configure server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const express = require("express"); // npm installed
-
+require('dotenv').config(); // allow server to read .env for environmental variables
+const api_key = process.env.API_KEY;
 const app = express();
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
