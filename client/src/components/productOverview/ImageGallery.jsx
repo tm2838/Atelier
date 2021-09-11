1cada1e8a492cefa0e74a2406ec1335d7628ce86
J <jbelding16@gmail.com>
2021-09-11 16:51:15 -0400

    <fixed> Changed class to className in component html

--- client/src/components/productOverview/ImageGallery.jsx
+++ client/src/components/productOverview/ImageGallery.jsx
@@ -5,28 +5,28 @@ const ImageGallery = (props) => {
     <div>
       {
         (props.currentStyle.length === 0)
-          ? <figure class='gallery-main'>
-              <img class='gallery-img' alt='Main image'></img>
+          ? <figure className='gallery-main'>
+              <img className='gallery-img' alt='Main image'></img>
             </figure>
 
-          : <div class='gallery'>
-            <figure class='gallery-main'>
-              <img src={props.currentStyle[0].photos[0].url} class='gallery-img' alt='Main image'></img>
+          : <div className='gallery'>
+            <figure className='gallery-main'>
+              <img src={props.currentStyle[0].photos[0].url} className='gallery-img' alt='Main image'></img>
             </figure>
-            <figure class='gallery-thumb-1'>
-              <img src={props.currentStyle[0].photos[1].thumbnail_url} class='gallery-thumb' alt='Main image'></img>
+            <figure className='gallery-thumb-1'>
+              <img src={props.currentStyle[0].photos[1].thumbnail_url} className='gallery-thumb' alt='Main image'></img>
             </figure>
-            <figure class='gallery-thumb-2'>
-              <img src={props.currentStyle[0].photos[2].thumbnail_url} class='gallery-thumb' alt='Main image'></img>
+            <figure className='gallery-thumb-2'>
+              <img src={props.currentStyle[0].photos[2].thumbnail_url} className='gallery-thumb' alt='Main image'></img>
             </figure>
-            <figure class='gallery-thumb-3'>
-              <img src={props.currentStyle[0].photos[3].thumbnail_url} class='gallery-thumb' alt='Main image'></img>
+            <figure className='gallery-thumb-3'>
+              <img src={props.currentStyle[0].photos[3].thumbnail_url} className='gallery-thumb' alt='Main image'></img>
             </figure>
-            <figure class='gallery-thumb-4'>
-             <img src={props.currentStyle[0].photos[4].thumbnail_url} class='gallery-thumb' alt='Main image'></img>
+            <figure className='gallery-thumb-4'>
+             <img src={props.currentStyle[0].photos[4].thumbnail_url} className='gallery-thumb' alt='Main image'></img>
             </figure>
-            <figure class='gallery-thumb-5'>
-              <img src={props.currentStyle[0].photos[5].thumbnail_url} class='gallery-thumb' alt='Main image'></img>
+            <figure className='gallery-thumb-5'>
+              <img src={props.currentStyle[0].photos[5].thumbnail_url} className='gallery-thumb' alt='Main image'></img>
             </figure>
             </div>
 
