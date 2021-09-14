4fd713cdbe9dc905976fb96bd97c3bfb90b99627
Surekha Weinberg <s.kaniget@gmail.com>
2021-09-13 19:33:01 -0700

    implement relatedProducts carousel skeleton

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './styles.css';
 import ProductReviews from './productReviews/productReviews.jsx';
+import RelatedProducts from './relatedProducts/RelatedProducts.jsx';
 import ProductOverviewContainer from './productOverview/ProductOverviewContainer.jsx';
 import './common/fontAwesomeIcons';
 
@@ -10,6 +11,7 @@ const App = () => (
   <div>
     <h1>Hello Atelier</h1>
     <ProductOverviewContainer />
+    <RelatedProducts />
     <ProductReviews />
   </div>
 );
