3b33ae38e110a8dab0524cb15db7777143fcc3c7
J <jbelding16@gmail.com>
2021-09-17 10:52:51 -0400

    <work in progress> click style image to change currentStyle

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -3,11 +3,13 @@ import reviews from './productReviews/reviewsReducer';
 import reviewMeta from './productReviews/reviewMetaReducer';
 import currentProduct from './productOverview/currentProduct';
 import styleList from './productOverview/styleList';
+import currentStyle from './productOverview/currentStyle';
 
 // example root reducer
 export default combineReducers({
   currentProduct,
   styleList,
+  currentStyle,
   reviews,
   reviewMeta,
 });
