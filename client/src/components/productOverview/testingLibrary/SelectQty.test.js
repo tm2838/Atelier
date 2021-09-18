347f30c5421768258273035f38cdf39013530153
J <jbelding16@gmail.com>
2021-09-18 16:14:24 -0400

    Added placeholder tests to empty test files

---
+++ client/src/components/productOverview/testingLibrary/SelectQty.test.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import fetchMock from 'jest-fetch-mock';
+import rootReducer from '../../../reducers/rootReducer';
+import testProduct from '../../../fixtures/testProduct.json';
+
+describe('Placeholder test', () => {
+  it('holds my place', () => {
+    expect(1).toEqual(1);
+  });
+});
