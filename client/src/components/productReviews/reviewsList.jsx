a9464d5e5f88855a19f457c159975de6ef78f52b
tingjunman <tm2838@nyu.edu>
2021-09-19 16:12:42 -0500

    style update

--- client/src/components/productReviews/reviewsList.jsx
+++ client/src/components/productReviews/reviewsList.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import Review from './review.jsx';
+import CSS from './productReviews.module.css';
 
 class ReviewsList extends React.Component {
   render() {
     return (
-      <div>
+      <div className={CSS['reviews-list']}>
         {this.props.loadedReviews.map(
           (review) => <Review key={review.review_id} review={review}/>,
         )}
