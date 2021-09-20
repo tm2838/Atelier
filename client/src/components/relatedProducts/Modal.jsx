b480fec75e641caf4a8dc1897c0298ebebc9eb9a
Surekha Weinberg <s.kaniget@gmail.com>
2021-09-20 12:02:11 -0700

    implement tests for modal rendering

--- client/src/components/relatedProducts/Modal.jsx
+++ client/src/components/relatedProducts/Modal.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 
@@ -9,38 +10,69 @@ const Modal = (props) => {
   if (comparedProduct.product !== undefined && comparedProduct.product.features !== undefined) {
     // eslint-disable-next-line no-plusplus
     for (let i = 0; i < currentProduct.features.length; i++) {
-      if (currentProduct.features[i].value === true) {
-        currentProduct.features[i].value = <FontAwesomeIcon icon={'faCheck'} />;
-      } else if (currentProduct.features[i].value === false) {
-        currentProduct.features[i].value = '';
-      }
       features[currentProduct.features[i].feature] = {
         currentProduct: currentProduct.features[i].value,
       };
     }
     // eslint-disable-next-line no-plusplus
     for (let j = 0; j < comparedProduct.product.features.length; j++) {
-      if (comparedProduct.product.features[j].value === true) {
-        comparedProduct.product.features[j].value = <FontAwesomeIcon icon={'faCheck'} />;
-      } else if (comparedProduct.product.features[j].value === false) {
-        comparedProduct.product.features[j].value = '';
+      if (!features[comparedProduct.product.features[j].feature]) {
+        features[comparedProduct.product.features[j].feature] = {
+          comparedProduct: comparedProduct.product.features[j].value,
+        };
+      } else {
+        features[comparedProduct.product.features[j].feature].comparedProduct = (
+          comparedProduct.product.features[j].value
+        );
       }
-      features[comparedProduct.product.features[j].feature] = {
-        comparedProduct: comparedProduct.product.features[j].value,
-      };
     }
   }
 
   const keys = Object.keys(features);
   // eslint-disable-next-line react/jsx-key
-  const rows = keys.map((key) => (
-      // eslint-disable-next-line react/jsx-key
-      <tr className='features'>
-        <td className='check'>{features[key].currentProduct}</td>
-        <td>{key}</td>
-        <td className='check'>{features[key].comparedProduct}</td>
+  const rows = keys.map((key) => {
+    if (features[key].currentProduct === true && features[key].comparedProduct === true) {
+      return (
+        <tr className='features'>
+          <td className='check'>
+            <FontAwesomeIcon data-testid='checkMark' icon={ faCheck } />
+          </td>
+          <td>{key}</td>
+          <td className='check'><FontAwesomeIcon data-testid='checkMark' icon={ faCheck } /></td>
+        </tr>
+      );
+    // eslint-disable-next-line no-else-return
+    } else if (features[key].comparedProduct === true) {
+      return (
+        <tr className='features'>
+          <td className='check'>{ features[key].currentProduct }</td>
+          <td>{key}</td>
+          <td className='check'>
+            <FontAwesomeIcon data-testid='checkMark' icon={ faCheck } />
+          </td>
+        </tr>
+      );
+    } else if (features[key].currentProduct === true) {
+      return (
+        <tr className='features'>
+          <td className='check'>
+            <FontAwesomeIcon data-testid='checkMark' icon={ faCheck } />
+          </td>
+          <td>{key}</td>
+          <td className='check'>{ features[key].comparedProduct }</td>
+        </tr>
+      );
+    } else {
+      return (
+        // eslint-disable-next-line react/jsx-key
+        <tr className='features'>
+          <td className='check'>{features[key].currentProduct}</td>
+          <td>{key}</td>
+          <td className='check'>{features[key].comparedProduct}</td>
       </tr>
-  ));
+      );
+    }
+  });
 
   if (props.showModal === false) {
     return null;
