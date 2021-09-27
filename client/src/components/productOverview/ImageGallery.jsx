4bb30a181eca41a71b61b430a7436e7d54a48da9
J <jbelding16@gmail.com>
2021-09-27 10:28:39 -0400

    <refactor> gallery thumbnails into a list component

--- client/src/components/productOverview/ImageGallery.jsx
+++ client/src/components/productOverview/ImageGallery.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import ImageMain from './ImageMain.jsx';
+import ImageList from './ImageList.jsx';
 import NavList from './ImageNavList.jsx';
 import ExpandButton from './ImageExButton.jsx';
+// import IconList from './ImageIconList.jsx';
 
 class ImageGallery extends React.Component {
   constructor(props) {
@@ -59,62 +61,15 @@ class ImageGallery extends React.Component {
         : <div className='gallery'>
           <ImageMain photo={this.props.currentStyle.photos[this.state.main].url} />
 
-          <figure className='gallery-thumb-0' onClick={() => this.handleImageClick(0)}>
-            <img
-              src={this.props.currentStyle.photos[this.state.thumb].thumbnail_url}
-              className='gallery-thumb'
-              alt='Thumbnail'>
-            </img>
-          </figure>
-
-          {
-            this.props.currentStyle.photos[1] && <figure className='gallery-thumb-1' data-testid='gallery-thumb-1'
-              onClick={() => this.handleImageClick(1)}>
-              <img
-                src={this.props.currentStyle.photos[this.state.thumb + 1].thumbnail_url}
-                className='gallery-thumb'
-                alt='Thumbnail'>
-              </img>
-            </figure>
-          }
-
-          {
-            this.props.currentStyle.photos[2] && <figure className='gallery-thumb-2' data-testid='gallery-thumb-2'
-              onClick={() => this.handleImageClick(2)}>
-              <img
-                src={this.props.currentStyle.photos[this.state.thumb + 2].thumbnail_url}
-                className='gallery-thumb'
-                alt='Thumbnail'>
-              </img>
-            </figure>
-          }
-
-          {
-            this.props.currentStyle.photos[3] && <figure className='gallery-thumb-3' data-testid='gallery-thumb-3'
-              onClick={() => this.handleImageClick(3)}>
-              <img
-                src={this.props.currentStyle.photos[this.state.thumb + 3].thumbnail_url}
-                className='gallery-thumb'
-                alt='Thumbnail'>
-              </img>
-            </figure>
-          }
-
-          {
-            this.props.currentStyle.photos[4] && <figure className='gallery-thumb-4' data-testid='gallery-thumb-4'
-              onClick={() => this.handleImageClick(4)}>
-              <img
-                src={this.props.currentStyle.photos[this.state.thumb + 4].thumbnail_url}
-                className='gallery-thumb'
-                alt='Thumbnail'>
-              </img>
-            </figure>
-          }
+          <ImageList
+          thumb={this.state.thumb}
+          handleImageClick={this.handleImageClick}
+          />
 
           <NavList
-          main={this.state.main}
-          thumb={this.state.thumb}
-          handleNavClick={this.handleNavClick}
+            main={this.state.main}
+            thumb={this.state.thumb}
+            handleNavClick={this.handleNavClick}
           />
 
           <ExpandButton />
--- client/src/components/productOverview/ImageList.jsx
+++ client/src/components/productOverview/ImageList.jsx
@@ -1,7 +1,71 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const ImageList = () => (
-  <div></div>
+const ImageList = (props) => (
+  <>
+    <figure className='gallery-thumb-0' onClick={() => props.handleImageClick(0)}>
+      <img
+        src={props.currentStyle.photos[props.thumb].thumbnail_url}
+        className='gallery-thumb'
+        alt='Thumbnail'>
+      </img>
+    </figure>
+
+    {
+      props.currentStyle.photos[1] && <figure className='gallery-thumb-1' data-testid='gallery-thumb-1'
+        onClick={() => props.handleImageClick(1)}>
+        <img
+          src={props.currentStyle.photos[props.thumb + 1].thumbnail_url}
+          className='gallery-thumb'
+          alt='Thumbnail'>
+        </img>
+      </figure>
+    }
+
+    {
+      props.currentStyle.photos[2] && <figure className='gallery-thumb-2' data-testid='gallery-thumb-2'
+        onClick={() => props.handleImageClick(2)}>
+        <img
+          src={props.currentStyle.photos[props.thumb + 2].thumbnail_url}
+          className='gallery-thumb'
+          alt='Thumbnail'>
+        </img>
+      </figure>
+    }
+
+    {
+      props.currentStyle.photos[3] && <figure className='gallery-thumb-3' data-testid='gallery-thumb-3'
+        onClick={() => props.handleImageClick(3)}>
+        <img
+          src={props.currentStyle.photos[props.thumb + 3].thumbnail_url}
+          className='gallery-thumb'
+          alt='Thumbnail'>
+        </img>
+      </figure>
+    }
+
+    {
+      props.currentStyle.photos[4] && <figure className='gallery-thumb-4' data-testid='gallery-thumb-4'
+        onClick={() => props.handleImageClick(4)}>
+        <img
+          src={props.currentStyle.photos[props.thumb + 4].thumbnail_url}
+          className='gallery-thumb'
+          alt='Thumbnail'>
+        </img>
+      </figure>
+    }
+  </>
 );
 
-export default ImageList;
+const mapStateToProps = (state) => ({
+  currentStyle: state.currentStyle,
+});
+
+ImageList.propTypes = {
+  currentStyle: PropTypes.object,
+  thumb: PropTypes.number,
+  handleImageClick: PropTypes.func,
+};
+
+export default connect(mapStateToProps)(ImageList);
