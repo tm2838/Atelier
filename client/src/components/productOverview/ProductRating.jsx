65887ac68107d7113eb7ff9a539019395914e4df
J <jbelding16@gmail.com>
2021-09-29 12:06:59 -0400

    <fix> correct number of stars in product rating weren't rendering

--- client/src/components/productOverview/ProductRating.jsx
+++ client/src/components/productOverview/ProductRating.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import StarRating from '../common/starRating.jsx';
 
-const ProductRating = (props) => (
+const ProductRating = ({ reviewMeta, reviewNumber }) => (
   <div className='overview-rating'>
-    <StarRating />
-    <a href='#ratings-and-reviews' className='rating-link' data-testid='rating-link'><u>Read all {props.reviewNumber} reviews</u></a>
+    <StarRating rating={reviewMeta.ratingScore}/>
+    <a className='rating-link' data-testid='rating-link'><u>Read all {reviewNumber} reviews</u></a>
   </div>
 );
 
+const mapStateToProps = (state) => ({
+  reviewMeta: state.reviewMeta,
+});
+
 ProductRating.propTypes = {
+  reviewMeta: PropTypes.object,
   reviewNumber: PropTypes.number,
 };
 
-export default ProductRating;
+export default connect(mapStateToProps)(ProductRating);
