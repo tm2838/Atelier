1772219e1b805a6c863226e19dbe654030e7464a
tingjunman <tm2838@nyu.edu>
2021-09-30 21:31:41 -0500

    add tests for helpers

--- client/src/components/ratingsAndReviews/test/review.test.jsx
+++ client/src/components/ratingsAndReviews/test/review.test.jsx
@@ -9,7 +9,7 @@ describe('Review', () => {
     const { getByText } = render(<Review review={testReview.reviews[0]}/>);
 
     expect(getByText(/This is a review body 1/)).toBeTruthy();
-    expect(getByText(/2019-01-11/)).toBeTruthy();
+    expect(getByText(/2021-01-11/)).toBeTruthy();
     expect(getByText(/This is a review summay 1/)).toBeTruthy();
     expect(getByText(/reviewer1/)).toBeTruthy();
   });
