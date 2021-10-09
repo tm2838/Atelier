5c9715e80ebd679b5152d08dbf88e7a8ef993845
J <jbelding16@gmail.com>
2021-10-08 21:28:13 -0400

    Added style to distinguish current main image in thumbnails

--- client/src/components/productOverview/Gallery/ThumbList.jsx
+++ client/src/components/productOverview/Gallery/ThumbList.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable consistent-return */
+/* eslint-disable array-callback-return */
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -6,73 +8,24 @@ const ThumbList = (props) => (
   <>
     {
       props.currentStyle.photos.map((photo, index) => {
-        const style = props.thumb + index === props.main ? { filter: 'blur(0)' } : { filter: 'blur(1px)' };
-        const photoIndex = index + props.thumb;
-        return (!!index < 5) && <figure
-        key={index * 9}
-        className={`gallery-thumb-${index}`}
-        data-testid={`gallery-thumb-${index}`}
-        onClick={() => props.handleImageClick(index)}>
-          <img
-            src={props.currentStyle.photos[photoIndex].thumbnail_url}
-            className='gallery-thumb'
-            style={style}
-            alt='Thumbnail'>
-          </img>
-        </figure>;
+        if (index < 5) {
+          const style = props.thumb + index === props.main ? { filter: 'grayscale(0)' } : { filter: 'grayscale(1)' };
+          const photoIndex = index + props.thumb;
+          return <figure
+            key={index * 9}
+            className={`gallery-thumb-${index}`}
+            data-testid={`gallery-thumb-${index}`}
+            onClick={() => props.handleImageClick(index)}>
+            <img
+              src={props.currentStyle.photos[photoIndex].thumbnail_url}
+              className='gallery-thumb'
+              style={style}
+              alt='Thumbnail'>
+            </img>
+          </figure>;
+        }
       })
     }
-    {/* <figure className='gallery-thumb-0' data-testid='gallery-thumb-0' onClick={() => props.handleImageClick(0)}>
-      <img
-        src={props.currentStyle.photos[props.thumb].thumbnail_url}
-        className='gallery-thumb'
-        alt='Thumbnail'>
-      </img>
-    </figure>
-
-    {
-      props.currentStyle.photos[1] && <figure className='gallery-thumb-1' data-testid='gallery-thumb-1'
-        onClick={() => props.handleImageClick(1)}>
-        <img
-          src={props.currentStyle.photos[props.thumb + 1].thumbnail_url}
-          className='gallery-thumb'
-          alt='Thumbnail'>
-        </img>
-      </figure>
-    }
-
-    {
-      props.currentStyle.photos[2] && <figure className='gallery-thumb-2' data-testid='gallery-thumb-2'
-        onClick={() => props.handleImageClick(2)}>
-        <img
-          src={props.currentStyle.photos[props.thumb + 2].thumbnail_url}
-          className='gallery-thumb'
-          alt='Thumbnail'>
-        </img>
-      </figure>
-    }
-
-    {
-      props.currentStyle.photos[3] && <figure className='gallery-thumb-3' data-testid='gallery-thumb-3'
-        onClick={() => props.handleImageClick(3)}>
-        <img
-          src={props.currentStyle.photos[props.thumb + 3].thumbnail_url}
-          className='gallery-thumb'
-          alt='Thumbnail'>
-        </img>
-      </figure>
-    }
-
-    {
-      props.currentStyle.photos[4] && <figure className='gallery-thumb-4' data-testid='gallery-thumb-4'
-        onClick={() => props.handleImageClick(4)}>
-        <img
-          src={props.currentStyle.photos[props.thumb + 4].thumbnail_url}
-          className='gallery-thumb'
-          alt='Thumbnail'>
-        </img>
-      </figure>
-    } */}
   </>
 );
 
